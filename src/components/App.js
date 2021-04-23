39ad871cfdd991eaadebe2f3c14da9578dc70662
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-23 10:37:36 -0700

    add control component to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import KegControl from "./KegControl";
 
 export default function App() {
   return(
     <>
       <Header />
+      <KegControl />
     </>
   );
 }
