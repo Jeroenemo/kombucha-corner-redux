a3d1c288d54d6ccf9ee31b891d7def9ff55289c4
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-23 10:52:09 -0700

    add new keg form component

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { v4 } from 'uuid';
+import FormTemplate from "./FormTemplate";
+import PropTypes from "prop-types";
+
+export default function NewKegForm(props) {
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({ 
+      name: event.target.name.value,
+      brand: event.target.brand.value,
+      price: event.target.price.value,
+      flavor: event.target.flavor.value,
+      quantity: 124,
+      id: v4()
+    });
+  }
+  return (
+    <>
+      <FormTemplate
+        formSubmissionHandler = { handleNewKegFormSubmission }
+        buttonText = "Add Keg" />
+    </>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
