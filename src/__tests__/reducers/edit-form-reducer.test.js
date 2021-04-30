d70c31ea6e54c82bc889eb131e85670a1c6274f4
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-30 14:44:01 -0700

    pass test for returning default state

---
+++ src/__tests__/reducers/edit-form-reducer.test.js
@@ -0,0 +1,9 @@
+import editFormReducer from "../../reducers/edit-form-reducer";
+import * as c from "./../../actions/ActionTypes";
+
+describe('editFormReducer', () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(editFormReducer(false, {type: null})).toEqual(false);
+  });
+});
