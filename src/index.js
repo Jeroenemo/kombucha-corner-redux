9c89f372eebb7c19bb1d8bf6b5961a042c676855
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-30 13:32:02 -0700

    instantiate redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './reducers/index';
+import './index.css';
+
+const store = createStore(rootReducer);
+
+store.subscribe(() =>
+  console.log(store.getState())
+);
 
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>,
+  </Provider>,
   document.getElementById('root')
 );
 
