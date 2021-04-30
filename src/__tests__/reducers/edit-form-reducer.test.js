79f65f37d6fe49c685a5094e50b14064725412c5
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-30 14:50:05 -0700

    pass test for toggling edit state to ture

--- src/__tests__/reducers/edit-form-reducer.test.js
+++ src/__tests__/reducers/edit-form-reducer.test.js
@@ -6,4 +6,8 @@ describe('editFormReducer', () => {
   test('Should return default state if no action type is recognized', () => {
     expect(editFormReducer(false, {type: null})).toEqual(false);
   });
+
+  test('Should toggle edit state to true', () => {
+    expect(editFormReducer(false, {type: c.TOGGLE_EDIT})).toEqual(true);
+  });
 });
